3c5ac89ae19012b6febe0c87347eac613cbbfe74
Andy-Villanueva-Guzman <villanueva.guzman.andy@gmail.com>
2021-11-22 11:53:07 -0600

    Ya esta todo

---
+++ app/src/main/java/com/andy/villanueva/calculadoraandy/MainActivity.java
@@ -0,0 +1,191 @@
+package com.andy.villanueva.calculadoraandy;
+
+import static java.lang.Math.cbrt;
+import static java.lang.Math.cos;
+import static java.lang.Math.log;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.toRadians;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    Double num1, num2, resultado;
+    String operador;
+
+    public void onClickBorrarTodo(View miView){
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("");
+    }
+
+    public void onClickOperacionCapturaNumero1(View miView){
+        TextView tv = (TextView) findViewById(R.id.textView);
+        num1 = Double.parseDouble(tv.getText().toString());
+        tv.setText("");
+    }
+
+    public void onClickSuma(View miView){
+        operador = "+";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickResta(View miView){
+        operador = "-";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickMulti(View miView){
+        operador = "*";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickDivicion(View miView){
+        operador = "/";
+        onClickOperacionCapturaNumero1(miView);
+    }
+    public void onClickRaiz(View miView){
+
+        operador = "root";
+    }
+
+    public void onClickLog(View miView){
+
+        operador = "log";
+    }
+
+    public void onClickExpo(View miView){
+        operador = "^";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickIgual(View miView){
+        TextView tv = (TextView) findViewById(R.id.textView);
+        num2 = Double.parseDouble(tv.getText().toString());
+
+        try{
+            if(operador.equals("+")){
+                resultado = num1 + num2;
+            }
+            else{
+                if(operador.equals("-")){
+                    resultado = num1 - num2;
+                }
+                else{
+                    if(operador.equals("/")){
+                        resultado = num1 / num2;
+                    }
+                    else{
+                        if(operador.equals("*")) {
+                            resultado = num1 * num2;
+                        }
+                        else{
+                            if(operador.equals("^")){
+                                resultado = pow(num1,num2);
+                            }
+                            else{
+                                if(operador.equals("root")){
+                                    resultado = Math.sqrt(num2);
+                                }
+                                else{
+                                    if(operador.equals("log")){
+                                        resultado = Math.log(num2);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            tv.setText(resultado.toString());
+        }
+        catch(NumberFormatException nfe){
+            Toast.makeText(this,"Numero Incorrecto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onClickButtonOne(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "1");
+    }
+
+    public void onClickButtonTwo(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "2");
+    }
+
+    public void onClickButtonThree(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "3");
+    }
+
+    public void onClickButtonFour(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "4");
+    }
+
+    public void onClickButtonFive(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "5");
+    }
+
+    public void onClickButtonSix(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "6");
+    }
+
+    public void onClickButtonSeven(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "7");
+    }
+
+    public void onClickButtonEight(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "8");
+    }
+
+    public void onClickButtonNine(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "9");
+    }
+
+    public void onClickButtonZero(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "0");
+    }
+    public void onClickcmamm(View miView){
+        operador = "cm a mm";
+        onClickOperacionCapturaNumero1(miView);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        try {
+            if (operador.equals("cm a mm")) {
+                resultado = num1 * 10;
+            }
+            tv.setText(resultado.toString());
+        } catch (NumberFormatException nfe) {
+            Toast.makeText(this, "Numero Incorrecto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
